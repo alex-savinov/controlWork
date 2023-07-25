bfaa8d6a3671b83eaa9c2583766a55470e4a537c
Alex <sp.savinov@gmail.com>
2023-07-25 12:32:58 +0300

    completed the solution of the task

--- exercise/Program.cs
+++ exercise/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] ReadLineToArray(string value)
+{
+    try
+    {
+        Console.WriteLine($"{value}: ");
+        string[] array = Console.ReadLine()!.Split().ToArray();
+        return array;
+    }
+    catch
+    {
+        string[] array = {""};
+        return array;
+    }
+}
+
+
+void Main()
+{
+    string[] userArray = ReadLineToArray("Введите обрабатываемый массив через пробел");
+    string[] newArray = new string[0];
+    for (int i = 0; i < userArray.Length; i++)
+    {
+        if (userArray[i].Length <= 3)
+        {
+            Array.Resize(ref newArray, newArray.Length + 1);
+            newArray[newArray.Length-1] = userArray[i];
+        }
+    }
+    
+   
+    Console.WriteLine($"[{string.Join("; ", newArray)}]");
+}
+
+Main();
