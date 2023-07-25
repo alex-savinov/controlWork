ef74debd941247f0fcb324cff5fe673bb5615c29
Alex <sp.savinov@gmail.com>
2023-07-25 13:27:22 +0300

    add comment for func, delete the magic number

--- exercise/Program.cs
+++ exercise/Program.cs
@@ -1,9 +1,9 @@
-﻿
+﻿//Конвертируем введенную пользователем строку в массив слов
 string[] ReadLineToArray(string value)
 {
     try
     {
-        Console.WriteLine($"{value}: ");
+        Console.Write($"{value}: ");
         string[] array = Console.ReadLine()!.Split().ToArray();
         return array;
     }
@@ -13,13 +13,13 @@ string[] ReadLineToArray(string value)
         return array;
     }
 }
-
-string[] ConvertArrayForTask(string[] userArray)
+//Функция принимает массив слов и искомую длину слов (по дефолту 3). Отсеивает слова
+string[] ConvertArrayForTask(string[] userArray, int maxLength = 3)
 {
     string[] newArray = new string[0];
     for (int i = 0; i < userArray.Length; i++)
     {
-        if (userArray[i].Length <= 3)
+        if (userArray[i].Length <= maxLength)
         {
             Array.Resize(ref newArray, newArray.Length + 1);
             newArray[newArray.Length - 1] = userArray[i];
